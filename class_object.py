6d2581e3eebaac8f71d4dbcd7d9051f482e7926c
bibek <badbond420@gmail.com>
2018-06-16 22:51:16 +0530

    class and object

---
+++ class_object.py
@@ -0,0 +1,12 @@
+class emp:
+    def __init__(self,fname,lname):
+        self.first_name = fname
+        self.last_name = lname
+        self.email = fname + "." + lname + "@domain.com"
+
+    def get_name(self):
+        return self.first_name + " " + self.last_name
+
+hari = emp("Hari", "Mantry")
+print(hari.get_name())
+print(hari.email)
