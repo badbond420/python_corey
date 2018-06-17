d47174f869b4f177c09691ff72f16c07ef64a0dc
bibek <badbond420@gmail.com>
2018-06-17 23:52:23 +0530

    some update in generator

--- generetor_stuff.py
+++ generetor_stuff.py
@@ -1,9 +1,11 @@
 def square_numbers(nums):
     for i in nums:
         yield (i*i)
+        print("lol") # it executes on 2nd call
 
 my_nums = square_numbers([1,2,3,4,5])
 print(my_nums)
+print(next(my_nums))
 print(next(my_nums))
 
 # for num in my_nums:
